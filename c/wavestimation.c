926aeabf4956d03b29cdfbed3263fbd0fc7e1aad
ThorntheLetter <ThorntheLetter@users.norepy.github.com>
2017-03-10 23:40:26 -0600

    added 2nd untested c function, moved comments to the wavestimation.h

--- c/wavestimation.c
+++ c/wavestimation.c
@@ -11,17 +11,38 @@
 // xs is garray pointing to input
 // returns a garray pointing to an array of garrays that contain arrays of output
 garray applyAlgs(garray fs, garray xs){
-	garray ret;
-	ret.size = sizeof(garray);
-	ret.length = fs.length;
-	ret.data = malloc(fs.length * sizeof(garray));
-	for(int i = 0; i < fs.length; i++){
-		((garray*)ret.data)[i].size = sizeof(garray);
-		((garray*)ret.data)[i].length = xs.length;
-		((garray*)ret.data)[i].data = malloc(xs.length * sizeof(garray));
-		for(int j = 0; j < xs.length; j++){
-			((garray*)((garray*)ret.data)[i].data)[j] = ((garray (**)(garray))fs.data)[i](((garray*)xs.data)[j]);
-		}
-	}
-	return ret;
+    garray ret;
+    ret.size = sizeof(garray);
+    ret.length = fs.length;
+    ret.data = malloc(fs.length * sizeof(garray));
+    for(int i = 0; i < fs.length; i++){
+        ((garray*)ret.data)[i].size = sizeof(garray);
+        ((garray*)ret.data)[i].length = xs.length;
+        ((garray*)ret.data)[i].data = malloc(xs.length * sizeof(garray));
+        for(int j = 0; j < xs.length; j++){
+            ((garray*)((garray*)ret.data)[i].data)[j] = ((garray (**)(garray))fs.data)[i](((garray*)xs.data)[j]);
+        }
+    }
+    return ret;
+}
+
+garray evalAlgs(garray eva, garray correct, garray results){
+    garray ret;
+    ret.size = sizeof(garray);
+    ret.length = results.length;
+    ret.data = malloc(results.length * sizeof(garray));
+    for(int i = 0; i < results.length; i++){
+        ((garray*)ret.data)[i].size = sizeof(garray);
+        ((garray*)ret.data)[i].length = eva.length;
+        ((garray*)ret.data)[i].data = malloc(eva.length * sizeof(garray));
+        for(int j = 0; j < eva.length; j++){
+            ((garray*)((garray*)ret.data)[i].data)[j].size = sizeof(garray);
+            ((garray*)((garray*)ret.data)[i].data)[j].length = correct.length;
+            ((garray*)((garray*)ret.data)[i].data)[j].data = malloc(correct.length * sizeof(garray));
+            for(int k = 0; k < correct.length; k++){
+                ((garray*)((garray*)((garray*)ret.data)[i].data)[j].data)[k] = ((garray (**)(garray, garray))eva.data)[j](((garray*)correct.data)[k], ((garray*)((garray*)results.data)[i].data)[k]);
+            }
+        }
+    }
+    return ret;
 }
--- c/wavestimation.h
+++ c/wavestimation.h
@@ -1,11 +1,29 @@
 #ifndef WAVESTIMATION_H
 #define WAVESTIMATION_H
+// Please do not use C for actual generic things.
+// If I were to actually use C I would rewrite this using actual types.
+// Seriously though, do not write C like this.
 
+// generic array, just call it gary
 typedef struct garray{
-    unsigned int size;
-    unsigned int length;
-    // unsigned int dims;
-    void* data;
+    unsigned int size; // size of an item in the array
+    unsigned int length; // number of items in array
+    void* data; // pointer to array data
 } garray;
+
+// UNTESTED, but it compiles
+// fs is garray with data pointing to an array of pointers to functions that take and return a garray
+// xs is garray pointing to input
+// returns a garray pointing to an array of garrays that contain arrays of output
+garray applyAlgs(garray fs, garray xs);
+
+// UNTESTED, but it compiles
+// eva is garray of pointers to the evaluation 
+// correct is a garray of the correct input, or other data needed to evaluate the solution
+// results is garray of garrays in the format that should be returned by applyAlgs
+// returns a garray of garrays of garrays formatted in a way that just look at one of the other sources
+garray evalAlgs(garray eva, garray correct, garray results);
+
+
 
 #endif
