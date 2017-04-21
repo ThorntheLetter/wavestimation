d2f851ed633f0775908eaa0050cce9c3376177ab
ThorntheLetter <ThorntheLetter@users.norepy.github.com>
2017-04-21 10:50:47 -0500

    added more experiment specific stuff, as well as first actual experiment test

--- python/wavestimation.py
+++ python/wavestimation.py
@@ -2,12 +2,14 @@ import numpy as np
 import experiment as e
 import multiprocessing as mp
 import json
+import wave
 
 POOL_SIZE = mp.cpu_count()
 
 #takes input frame and width of sample in bytes and transforms it into a number between -1 and 1, assuming it is signed
+#might want to change these to be based on range instead of size
 def squash(input, width = 2):
-    return((input / (2 ** ((8 * width) - 1))) - 1)
+    return(input / (2 ** ((8 * width) - 1)))
 
 #reverses squash()
 def unsquash(input, width = 2):
@@ -18,24 +20,23 @@ vunsquash = np.vectorize(unsquash)
 
 
 class sample():
-    def __init__(self, target = None, samples = None, json = None):
-        if(json): self.fromjson
+    def __init__(self, target = None, components = None, json = None):
+        if(json): self.fromjson(json); return   
         self.target = target #might want to see if it would be easy to insure correct numpy array types
-        self.samples = samples
+        self.components = 
 
     def fromjson(self, filename):
-        json_data = json.load(open(filename))
-        filename.close()
+        json_file = open(filename)
+        json_data = json.load(json_file)
+        json_file.close()
         targetfile = wave.open(json_data['target'])
-        self.target = np.fromstring(targetfile.readframes(file.getnframes()),dtype = 'int16')
-        self.target = vsquash(self.target)
+        self.target = vsquash(np.fromstring(targetfile.readframes(targetfile.getnframes()), dtype = 'int16'))
         targetfile.close()
-        samplefiles = json_data['samples']
-        samplefiles = map(wave.open, samplefiles)
-        self.samples = np.zeros((len(self.target), len(samplefiles)))
-        for s, i in enumerate(samplefiles):
-            sample = vsquash(np.fromstring(x.readframes(file.getnframes()), dtype = 'int16'))
-            self.samples[i] = sample[i,:len(target)]
+        samplefiles = list(map(wave.open, json_data['components']))
+        self.components = np.zeros((len(samplefiles), len(self.target)))
+        for i, s in enumerate(samplefiles):
+            sample = vsquash(np.fromstring(s.readframes(s.getnframes()), dtype = 'int16'))
+            self.components[i] = sample[:len(self.target)]
 
 def main():
     pool = mp.Pool(processes = POOL_SIZE)
